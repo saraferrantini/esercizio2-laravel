79cf153f6a7beea8ee5f01be01655eb10bf47675
saraferrantini <sara.ferrantini@gmail.com>
2024-05-20 17:14:56 +0200

    aggiunte

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -1,38 +1,82 @@
 <?php
 
-
 namespace App\Http\Controllers;
+
+use App\Models\Book;
 use Illuminate\Http\Request;
 
-
 class BookController extends Controller
-
 {
-
-
-
     public function index()
     {
-        return view('books.create');
+        // dal db prendiamo la lista dei libri
+        // $books = Book::all();
+        $books = Book::paginate(); // default 15 per pagina
+        // dd($books);
+        // dump($books);
+        // ddd($books);
+
+        // $myVar = Book::where('price', 153)->get();
+        // $myVar = Book::where('price', '>', 200)->get();
+        $searchTerm = 'lorem';
+        // $books = Book::where('title', 'LIKE', "%$searchTerm%")->orderBy('price', 'asc')->limit(3)->offset(2)->get();
+
+        // $books = Book::where('title', 'LIKE', "%$searchTerm%")->paginate(5);
+
+        // $myVar = Book::where('price', 153)->max('id');
+        // $myVar = Book::where()->count();
+        // $myVar = Book::where()->max('price');
+        // dump($books);
+
+        return view('books.index', [
+            'books' => $books,
+        ]);
     }
 
     public function create()
     {
         return view('books.create');
     }
+
     public function show($id)
     {
-        return view('books,show',[
-            'id' =>$id
-        ]
-        
-        );
+        $book = Book::findOrFail($id);
+        // $book = Book::firstWhere('price', 153);
+        // dd($book);
 
-}
+        return view('books.show', [
+            'book' => $book
+        ]);
+    }
 
-public function edit($id)
-{
-    return view('books.edit', compact('id'));
-}
+    public function edit($id)
+    {
+        return view('books.edit', compact('id'));
+    }
 
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        // dd($data);
+        // salvare i dati nel database
+        $newBook = new Book();
+        $newBook->title = $data['title'];
+        $newBook->author = $data['author'];
+        $newBook->price = $data['price'];
+        $newBook->img = $data['img'];
+        $newBook->save();
+
+        // Book::insert
+
+        // ridirezionare
+        return redirect()->route('books.index');
+    }
+
+    public function destroy($id)
+    {
+        $book = Book::findOrFail($id);
+        $book->delete();
+
+        return redirect()->route('books.index');
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,27 +2,19 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-*/
-  public function run(): void{// User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-        DB::table('books')->insert([
-            'title' => Str::random(rand(20,50)),
-            'price' =>  rand(100, 350),
-            'author'=> Str::random(10),
-            'img' => Str::random(10),
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call([
+            UsersSeeder::class,
+            BooksSeeder::class,
         ]);
     }
 }
